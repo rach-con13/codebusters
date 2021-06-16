3a3177ead6dd87ad56f80c430e9f8eb83aad45a8
rach-con13 <rachaelconcessio+@gmail.com>
2021-06-16 16:06:32 -0400

    created header section

---
+++ components/Header/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className="w-full">
+          <div className="w-10/12 py-4">
+            <h1 className="text-lg bg-white">Codebusters</h1>
+            <nav className="text-base">
+             <p className="">Login</p>
+             <p className="ml-4">Sign Up</p>
+            </nav>
+          </div>
+        </header>
+    )
+}
---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="w-lg">
+
+        </div>
+    )
+}
